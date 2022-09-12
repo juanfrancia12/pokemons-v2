61e441012d6bac80d12d74a6c8faceeeae7c291a
Juan Francia <jf2381886@gmail.com>
2022-09-12 13:07:11 -0500

    Estructura inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from "next";
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-300">Hello world!</h1>
-  );
-};
+    <h1 className="text-3xl font-bold underline text-red-300">
+      ESTE ES UN TITULO NUEVO
+    </h1>
+  )
+}
 
-export default Home;
+export default Home
