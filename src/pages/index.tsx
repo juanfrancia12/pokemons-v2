7617a1d54318fffdfb78278a124476b4f2c34751
Juan Francia <jf2381886@gmail.com>
2022-09-12 13:11:32 -0500

    Actualizando .gitignore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next"
 const Home: NextPage = () => {
   return (
     <h1 className="text-3xl font-bold underline text-red-300">
-      ESTE ES UN TITULO NUEVO
+      ESTE ES UN TITULO NUEVO Y ES DE NEXT JS
     </h1>
   )
 }
