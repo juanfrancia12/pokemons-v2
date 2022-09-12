ec690e69e572c2d69860d48f40cf496bb06855da
Juan Francia <jf2381886@gmail.com>
2022-09-12 13:20:13 -0500

    Actualizado .gitignore

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,11 +1,7 @@
 type Props = {}
 
 const Home = (props: Props) => {
-<<<<<<< HEAD
   return <div>Pagina principal</div>
-=======
-  return <div>Home pageeee</div>
->>>>>>> e8c2327734a4ca1925f23d3167e63a28bdf5c355
 }
 
 export default Home
